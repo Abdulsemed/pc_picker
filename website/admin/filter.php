a9687ea250c73279341bfc49a92f44f8f87e21d9
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2024-01-10 22:40:45 +0300

    change filtering from radio button to slider

--- website/admin/filter.php
+++ website/admin/filter.php
@@ -1,4 +1,4 @@
-<form action="landing_page.php" method="get">
+<!-- <form action="landing_page.php" method="get">
 	<ul class="list-group mb-3">
 		<p class="cardHeader"> By price</p>
 		<li class="list-group-item d-flex justify-content-between lh-condensed">
@@ -51,4 +51,42 @@
 		<br>
 		<input type="submit" class="btn btn-primary" value="Submit">
 	</ul>
+</form> -->
+
+<form action="landing_page.php" method="get">
+    <ul class="list-group mb-3">
+        <p class="cardHeader">By price</p>
+        <li class="list-group-item d-flex justify-content-between lh-condensed">
+            <div>
+                <label for="priceSlider">Select Price Range: </label>
+                <input type="range" class="price" name="price" id="priceSlider" min="500" max="2500" step="500" value="1000" oninput="submitForm()">
+                <output for="priceSlider">$<span id="priceOutput">1000</span></output>
+            </div>
+        </li>
+        <br>
+        <p class="cardHeader">RAM size</p>
+        <li class="list-group-item d-flex justify-content-between lh-condensed">
+            <div>
+                <label for="ramSlider">Select RAM Size: </label>
+                <input type="range" class="RAM" name="RAM" id="ramSlider" min="8" max="64" step="8" value="8" oninput="submitForm()">
+                <output for="ramSlider"><span id="ramOutput">8</span>GB</output>
+            </div>
+        </li>
+        <br>
+        <input type="submit" class="btn btn-primary" value="Submit">
+    </ul>
 </form>
+
+<script>
+    function submitForm() {
+        document.forms[0].submit();
+    }
+
+    document.getElementById('priceSlider').addEventListener('input', function () {
+        document.getElementById('priceOutput').textContent = this.value;
+    });
+
+    document.getElementById('ramSlider').addEventListener('input', function () {
+        document.getElementById('ramOutput').textContent = this.value;
+    });
+</script>
